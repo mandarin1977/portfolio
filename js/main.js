06f22885b1ca00c3e424618c1bf418349cf78ebb
mandarin1977 <57694287+mandarin1977@users.noreply.github.com>
2022-03-27 22:31:47 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.sliderW').slick({
+    prevArrow: '<button class="slick-prev">이전</button>',
+    nextArrow: '<button class="slick-next">다음</button>'
+});
+
+$('.popupbtn').click(function(){
+    $('.popup').hide()
+});
